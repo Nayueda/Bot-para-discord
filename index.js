b414a00bfe59ff69228fe42403c94d8efe2be976
Nayara Ueda <nayaraueda@gmail.com>
2023-02-19 21:49:47 -0300

    add comentário

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 //dotenv
 const dotenv = require ('dotenv');
 dotenv.config();
+
+// process.env é onde vai pegar as variáveis que sao constantes do arquivo env
 const{ TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -10,6 +12,5 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
 
 client.login(TOKEN);
